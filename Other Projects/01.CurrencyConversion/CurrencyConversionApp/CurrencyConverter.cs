cf62f14e5b8300f22be73b20fce86918c026bd95
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-10-07 14:23:55 +0300

    CurrencyConversionApp added (07.10.2023)

---
+++ Other Projects/01.CurrencyConversion/CurrencyConversionApp/CurrencyConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Xml;
+
+namespace CurrencyConversionApp
+{
+    public class CurrencyConverter
+    {
+        private XmlDocument exchangeRateData;
+
+        public CurrencyConverter(string exchangeRateDataUrl)
+        {
+            using (WebClient client = new WebClient())
+            {
+                string xmlData = client.DownloadString(exchangeRateDataUrl);
+                exchangeRateData = new XmlDocument();
+                exchangeRateData.LoadXml(xmlData);
+            }
+        }
+
+        public void ShowAvailableCurrencies()
+        {
+            XmlNodeList currencyNodes = exchangeRateData.SelectNodes("/Tarih_Date/Currency");
+            Console.WriteLine("Available Currencies:");
+            foreach (XmlNode currencyNode in currencyNodes)
+            {
+                string kod = currencyNode.Attributes["Kod"].Value;
+                string isim = currencyNode.SelectSingleNode("Isim").InnerText;
+                Console.WriteLine($"{kod} - {isim}");
+            }
+        }
+
+        public double ConvertCurrency(double amount, string sourceCurrency, string targetCurrency)
+        {
+            sourceCurrency = sourceCurrency.ToUpper();
+            targetCurrency = targetCurrency.ToUpper();
+
+            XmlNode sourceCurrencyNode = exchangeRateData.SelectSingleNode($"/Tarih_Date/Currency[@Kod='{sourceCurrency}']");
+            XmlNode targetCurrencyNode = exchangeRateData.SelectSingleNode($"/Tarih_Date/Currency[@Kod='{targetCurrency}']");
+
+            if (sourceCurrencyNode != null && targetCurrencyNode != null)
+            {
+                double sourceExchangeRate = double.Parse(sourceCurrencyNode.SelectSingleNode("ForexBuying").InnerText);
+                double targetExchangeRate = double.Parse(targetCurrencyNode.SelectSingleNode("ForexSelling").InnerText);
+
+                double result = (amount * sourceExchangeRate) / targetExchangeRate;
+                return result;
+            }
+            else
+            {
+                throw new ArgumentException("Invalid currency codes.");
+            }
+        }
+    }
+}
---
+++ Other Projects/01.CurrencyConversion/CurrencyConversionApp/Program.cs
@@ -0,0 +1,86 @@
+﻿using CurrencyConversionApp;
+using System;
+using System.Collections.Generic;
+
+class User
+{
+    public List<CurrencyConversion> Conversions { get; set; }
+
+    public User()
+    {
+        Conversions = new List<CurrencyConversion>();
+    }
+
+    public void Start()
+    {
+        string exchangeRateDataUrl = "https://www.tcmb.gov.tr/kurlar/today.xml";
+        CurrencyConverter converter = new CurrencyConverter(exchangeRateDataUrl);
+
+        Console.WriteLine("Currency Conversion App");
+
+        converter.ShowAvailableCurrencies();
+
+        while (true)
+        {
+            Console.Write("Enter the amount to convert (or press 'q' to quit): ");
+            string input = Console.ReadLine();
+
+            if (input == "q")
+                break;
+
+            if (double.TryParse(input, out double amount))
+            {
+                Console.Write("Enter the source currency code: ");
+                string sourceCurrency = Console.ReadLine();
+
+                Console.Write("Enter the target currency code: ");
+                string targetCurrency = Console.ReadLine();
+
+                try
+                {
+                    double result = converter.ConvertCurrency(amount, sourceCurrency, targetCurrency);
+                    Console.WriteLine($"{amount} {sourceCurrency} = {result} {targetCurrency}");
+
+                    Conversions.Add(new CurrencyConversion
+                    {
+                        Amount = amount,
+                        SourceCurrency = sourceCurrency,
+                        TargetCurrency = targetCurrency,
+                        Result = result
+                    });
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid amount entered.");
+            }
+        }
+
+        Console.WriteLine("Conversions Made:");
+        foreach (var conversion in Conversions)
+        {
+            Console.WriteLine($"{conversion.Amount} {conversion.SourceCurrency} = {conversion.Result} {conversion.TargetCurrency}");
+        }
+    }
+}
+
+class CurrencyConversion
+{
+    public double Amount { get; set; }
+    public string SourceCurrency { get; set; }
+    public string TargetCurrency { get; set; }
+    public double Result { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        User user = new User();
+        user.Start();
+    }
+}
