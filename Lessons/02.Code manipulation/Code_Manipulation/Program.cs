0a85b8a360aae5242bd131fe18d86338e40b4f24
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-10-12 10:57:19 +0300

    Code manipulation added (12.10.2023)

---
+++ Lessons/02.Code manipulation/Code_Manipulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Welcome
+{
+
+    static void Main()
+    {
+        Console.Write("Thats a console line");
+        Console.WriteLine("Thats a console line");
+        string userInput = Console.ReadLine();
+        Console.WriteLine($"Thats a console line");
+        int nuber1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine($"{nuber1+num2}");
+
+
+
+    }
+
+
+}
