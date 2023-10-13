4848b378a1e7b685751712b8a09bf7491d9014ca
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-10-13 16:11:08 +0300

    ConsoleApp added (13.10.2023)

---
+++ 02.ConsoleApp/CnslApp.Presentation/ConsoleApp.Presentation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Presentation
+{
+    static void Main()
+    {
+        bool check = true;
+        while (check)
+        {
+
+            Console.WriteLine("Enter a five-digit number:");
+            string userInput = Console.ReadLine();
+
+            if (userInput.Length == 5)
+            {
+                for (int i = 0; i < userInput.Length; i++)
+                {
+                    if (i == userInput.Length)
+                        Console.Write($"{userInput[i].ToString()}");
+                    Console.Write($"{userInput[i]}   ");
+
+                }
+                check = !true;
+                break;
+            }
+            Console.WriteLine("Error!! u must be enter 5 diget number");
+        }
+    }
+}
