b0b6f7d73e7eb19dd2da4a4205650f28b3122217
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-10-19 21:58:22 +0300

    Private and public added (19.10.2023)

---
+++ 03.Private-public/Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using Classes;
+using System;
+class Program
+{
+
+    #region First 
+    // static int Main(    )
+    //{
+    //    //class ile object in farkı nedir
+
+    //    Account account = new() { Name = "Deam" };
+    //    account.Name = "DEamm";
+    //    Console.WriteLine($"{account.Name}");
+
+
+    //    Console.ReadLine();
+    //    return 0;
+    //}
+
+    #endregion
+
+
+    static void Main()
+    {
+        Account myAccount = new Account();
+        Console.WriteLine($"Initial name is: {myAccount.GetName()}");
+        Console.Write("Enter the name: "); 
+        string theName = Console.ReadLine(); 
+
+        myAccount.SetName (theName); 
+        Console.WriteLine($"myAccount's name is: {myAccount.GetName()}");
+    }
+}
