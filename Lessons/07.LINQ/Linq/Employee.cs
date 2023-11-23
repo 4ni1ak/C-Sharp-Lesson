48a8946eeef6bf33912ea25193a673676975c4b6
Anıl Akpınar <4ni1ak+github@gmail.com>
2023-11-23 12:07:24 +0300

    [Feature]: Added LINQ and List examples
    Added examples for Ex0 and Ex1. Employee class is utilized to create employees and perform filtering based on specific criteria.
    
    Added example for Ex2. Filtering is performed on an array using the "where" clause.
    
    Added examples for Ex3, Ex4, and Ex5. Sorting and filtering of array elements are demonstrated.
    
    Added examples for Ex6 and Ex7. Various operations are performed on a List<string>.

---
+++ Lessons/07.LINQ/Linq/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private decimal monthlySalary;
+        
+        public Employee(string firstName, string lastName,decimal monthlySalary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MonthlySalary = monthlySalary;
+        }
+        public decimal MonthlySalary
+        {
+            get => monthlySalary;
+            set
+            {
+                if (value >= 0M)
+                {
+                    monthlySalary = value;
+                }
+            }
+        }
+
+        public override string ToString() => $"{FirstName,-10}{LastName.Length,-10}{MonthlySalary,10:C}";
+    }
+}
---
+++ Lessons/07.LINQ/Linq/Program.cs
@@ -0,0 +1,146 @@
+﻿using Linq;
+
+class Program
+{
+    static void Main()
+    {
+        #region Ex0
+
+        
+
+        var employees = new[]
+        {
+            new Employee("Jatson","Green",500M),
+            new Employee("Ali","Green",400M),
+            new Employee("Hakan","Green",300M),
+            new Employee("Hüseyin","Blue",200M),
+            new Employee("Nigar","Red",100M),
+            new Employee("Ice","Green",900M),
+        };
+        foreach (var e in employees)
+        {
+            Console.WriteLine(e);
+            
+        }
+
+        var between4k6k = employees.Where(x => x.MonthlySalary >= 4000M && x.MonthlySalary <= 6000M).ToList();
+
+        foreach (var employe in between4k6k)
+        {
+            Console.Write(employe);
+        }
+
+        var nameSorted = employees.OrderBy(x => x.LastName).ThenBy(x => x.FirstName).ToList();
+        
+        foreach (var employee in nameSorted)
+        {
+            Console.WriteLine($"{employee.LastName}, {employee.FirstName}");
+        }
+        #endregion
+
+        #region Ex1
+
+
+
+
+        var values = new[] { 20, 2, 4, 4, 66, 6, 65449, 7 };
+            Console.WriteLine("Oirginal Array");
+            foreach (var value in values)
+            {
+                Console.Write($"{value}, ");
+            }
+
+    
+        #endregion
+
+        #region Ex2
+
+        var filtered =
+            from value in values
+            where value > 4
+            select value;
+
+        foreach (var filteredValue in filtered)
+        {
+            Console.Write(filteredValue);
+        }
+        #endregion
+
+        #region Ex3
+
+        var sorted =
+            from value in values
+            orderby value
+            select value;
+        foreach (var element in sorted)
+        {
+            Console.Write(element);
+        }
+
+
+
+        //imparating and recariative
+
+        #endregion
+
+        #region Ex4
+        var sortedDis =
+            from value in values
+            orderby value descending 
+            select value;
+        int i=1;
+        
+        foreach (var element in sortedDis)
+        {
+            Console.Write(element);
+        }
+        #endregion
+
+        #region Ex5
+        var sortedfil =
+            from value in values
+            where value > 4
+            orderby value
+            select value;
+        foreach (var element in sortedfil)
+        {
+            Console.Write(element);
+        }
+        #endregion
+
+        //IEnumerable<T>
+        //Employee 
+
+        #region Ex6
+
+        var items =new List<string>();
+        Console.WriteLine("before adding to items:"+$"Count={items.Count}; Capacity = {items.Capacity}");
+        items.Add("red");
+        items.Insert(0,"yelloq");
+        Console.WriteLine("afrer adding to items:" + $"Count={items.Count}; Capacity = {items.Capacity}");
+        items.Remove("yellow");
+        items.RemoveAt(1);
+
+
+
+        #endregion
+
+        #region Ex7
+
+        var Items=new List<string>();
+        items.Add("aQua");
+        items.Add("Rust ");
+        items.Add("yeLLOw");
+        items.Add("rED");
+        var startsWithR = items.Select(item => (UppercaseString: item.ToUpper(), OriginalString: item))
+            .Where(x => x.UppercaseString.StartsWith("R"))
+            .OrderBy(x => x.UppercaseString)
+            .Select(x => x.UppercaseString);
+        foreach (var item in startsWithR)
+        {
+            Console.WriteLine(item);
+        }
+        #endregion
+
+    }
+}
