46e9e0aa2aae855c60e81224121085eae7cc1711
Anıl Akpınar <4ni1ak+github@gmail.com>
2024-01-10 20:49:46 +0300

    [Update]: ConvertToTitleCase added

--- Lessons/11.ExtensionMethods/ExtensionMethods.Presentation/Program.cs
+++ Lessons/11.ExtensionMethods/ExtensionMethods.Presentation/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Text;
 
 public static class StringExtensions
 {
@@ -27,6 +27,34 @@ public static class StringExtensions
     {
         return str.Length <= maxLength ? str : str.Substring(0, maxLength);
     }
+
+    public static string Reverse(this string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    public static string ConvertToTitleCase(this string str)
+    {
+        string[] words = str.Split(' ');
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (!string.IsNullOrWhiteSpace(words[i]))
+            {
+                char[] letters = words[i].ToCharArray();
+                letters[0] = char.ToUpper(letters[0]);
+                words[i] = new string(letters);
+            }
+        }
+        return string.Join(' ', words);
+    }
+
+    public static string EncodeBase64(this string str)
+    {
+        byte[] bytes = Encoding.UTF8.GetBytes(str);
+        return Convert.ToBase64String(bytes);
+    }
 }
 
 class Program
@@ -43,5 +71,14 @@ class Program
 
         string truncatedText = text.Truncate(20);
         Console.WriteLine($"Truncated Text: {truncatedText}");
+
+        string reversedText = text.Reverse();
+        Console.WriteLine($"Reversed Text: {reversedText}");
+
+        string titleCaseText = text.ConvertToTitleCase();
+        Console.WriteLine($"Title Case Text: {titleCaseText}");
+
+        string base64EncodedText = text.EncodeBase64();
+        Console.WriteLine($"Base64 Encoded Text: {base64EncodedText}");
     }
 }
