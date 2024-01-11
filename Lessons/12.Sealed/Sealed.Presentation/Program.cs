5538034ae60c883fb3997e698cdcc5e65f0acaf5
Anıl Akpınar <4ni1ak+github@gmail.com>
2024-01-11 22:34:05 +0300

    [Feature]: Sealed folder added

---
+++ Lessons/12.Sealed/Sealed.Presentation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+sealed class OuterClass
+{
+    public sealed class SealedInnerClass
+    {
+        public void SealedMethod()
+        {
+            Console.WriteLine("SealedMethod is called from SealedInnerClass.");
+        }
+    }
+
+    sealed class DerivedSealedInnerClass
+    {
+        public void UseSealedMethod()
+        {
+            Console.WriteLine("UseSealedMethod is called from DerivedSealedInnerClass.");
+        }
+    }
+
+
+    static void Main()
+    {
+        SealedInnerClass sealedInnerInstance = new SealedInnerClass();
+        sealedInnerInstance.SealedMethod();
+
+        DerivedSealedInnerClass derivedInstance = new DerivedSealedInnerClass();
+        derivedInstance.UseSealedMethod();
+    }
+}
